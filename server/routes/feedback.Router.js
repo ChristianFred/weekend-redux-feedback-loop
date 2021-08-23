c7ce0369a673b173291f9992895b0dfc9ec55bb3
Christian Fredricks <Christianpfred@gmail.com>
2021-08-23 14:41:38 -0500

    Reworked the Routes page to better show post and get

--- server/routes/feedback.Router.js
+++ server/routes/feedback.Router.js
@@ -2,25 +2,33 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    console.log('GET prime_feedback');
+    pool.query('SELECT * FROM "prime_feedback";').then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('GET error prime_feedback', error)
+        res.sendStatus(500);
+
+    });
+
+})
+
 // POST the feedback to the database
 router.post('/', (req, res) => {
-    console.log(req.body);
-    const feelings = feelings;
-    const understanding = understanding;
-    const support = support;
-    const comments = comments;
-    const sqlText = (`INSERT INTO "prime_feedback" ("feeling", "understanding", "support", "comments")
-        VALUES ($1, $2, $3, $4)
-        RETURNING id;`, [feeling, understanding, support, comments]);
-
-    pool.query(sqlText)
+    const newItem = req.body;
+    const sqlText = `INSERT INTO prime_feedback (feeling, understanding, support, comments)
+        VALUES ($1, $2, $3, $4)`;
+    pool.query(sqlText, [newItem.feelings, newItem.understanding, newItem.support, newItem.comments])
         .then((result) => {
+            console.log('Added feedback to the DB', newItem);
             res.sendStatus(201);
         })
         .catch((error) => {
-            console.log(`Error making database query error`);
+            console.log(`Error making database query ${sqlText}`, error);
             res.sendStatus(500);
-        });
-});
+
+        })
+})
 
 module.exports = router;
